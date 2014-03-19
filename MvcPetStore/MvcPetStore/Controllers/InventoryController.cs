a23f81cf12547196f02be70609538e25b8ed6bb8
lswroc01 <laura.wrocklage@gmail.com>
2014-03-19 13:06:28 -0400

    Added Inventory Controller; removed unecessary image files from main dir

---
+++ MvcPetStore/MvcPetStore/Controllers/InventoryController.cs
@@ -0,0 +1,27 @@
+﻿using MvcPetStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcPetStore.Controllers
+{
+    public class InventoryController : Controller
+    {
+        private PetStoreDBContext db = new PetStoreDBContext();
+        //
+        // GET: /PetInventory/
+        public ActionResult Index()
+        {
+            return View(db.Pets.ToList());
+        }
+
+        public ActionResult Details(int ID = 1)
+        {
+            ViewBag.ID = ID;
+
+            return View(db.Pets.ToList());
+        }
+    }
+}
