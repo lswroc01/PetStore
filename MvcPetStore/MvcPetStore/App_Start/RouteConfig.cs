0bbc05522250724e6d36ac3526889d2ea89db532
lswroc01 <laura.wrocklage@gmail.com>
2014-03-19 12:24:33 -0400

    update route for PetInventory

--- MvcPetStore/MvcPetStore/App_Start/RouteConfig.cs
+++ MvcPetStore/MvcPetStore/App_Start/RouteConfig.cs
@@ -18,6 +18,12 @@ namespace MvcPetStore
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            routes.MapRoute(
+                name: "PetInventory",
+                url: "{controller}/{action}/{name}/{id}"
+            );
+
         }
     }
 }
