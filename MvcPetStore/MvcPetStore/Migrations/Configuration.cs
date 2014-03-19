1a5fd93577b44321c9f49f796c719296b345d3d1
lswroc01 <laura.wrocklage@gmail.com>
2014-03-19 13:19:04 -0400

    fixed database connection issues

---
+++ MvcPetStore/MvcPetStore/Migrations/Configuration.cs
@@ -0,0 +1,31 @@
+namespace MvcPetStore.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<MvcPetStore.Models.PetStoreDBContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(MvcPetStore.Models.PetStoreDBContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
